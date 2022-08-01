b929379b6293e141daffd77ab5cc6efea8415632
André <alcbcoelho@gmail.com>
2022-08-01 18:23:39 -0300

    reprogrammed carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,92 +1,84 @@
+/////////////////////////////////////////////////
+// VARIABLES
+/////////////////////////////////////////////////
+
+const slideDuration = 3000;
+const carousel = document.querySelector(".carousel");
+const bar = document.querySelector(".carousel-bar-fill");
+const dots = document.querySelectorAll(".carousel-dot");
+const slideImgs = [
+"url(./media/slide-1.jpg)",
+"url(./media/slide-2.jpg)",
+"url(./media/slide-3.jpg)"
+];
+const totalSlides = dots.length;
+
 let slideNum = 0;
 
-const carousel = document.getElementById("carousel");
-const dots = Array.from(document.querySelectorAll(".carousel-dot"));
-const cx = getComputedStyle(carousel).backgroundImage;/*getComputedStyle(carousel).background;*/
-const bgImage = ["url(./media/slide-1.jpg)", "url(./media/slide-2.jpg)", "url(./media/slide-3.jpg)"];
-const bgPositionX = ["100%", "50%", "0%"];
-const bgColor = ["#eee", "#000", "#ff0000"];
+/////////////////////////////////////////////////
+// FUNCTIONS
+/////////////////////////////////////////////////
 
-nextSlide();
+function changeSlide() {
+    carousel.style.backgroundImage = slideImgs[slideNum];
+    dots.forEach(dot => dot.removeAttribute("id"));
+    dots[slideNum].id = "active";
+}
 
 function nextSlide() {
-
-    if (slideNum < dots.length) {
-        slideNum++;
-    }
-    else {
-        slideNum = 1;
-    }
-    carousel.style.backgroundImage = bgImage[slideNum - 1];
-    /*alert(slideNum);*/
-    dots[slideNum - 1].style.backgroundColor = "#943f42";
-    if (slideNum >= 2) {
-        dots[slideNum - 2].removeAttribute("style");
-    }
-    else {
-        if (dots[dots.length - 1].hasAttribute("style")) {
-            dots[dots.length - 1].removeAttribute("style");
-        }
-    }
+    if (!((slideNum + 1) % totalSlides)) {
+        slideNum = 0;
+    } else slideNum++;
+    changeSlide();
 }
 
 function previousSlide() {
-    if (slideNum > 1) {
-        slideNum--;
-    }
-    else {
-        slideNum = 3;
-    }
-    alert(slideNum);
-    for (let i = 0; i < dots.length; i++)
-    {
-        if (dots[slideNum - 1])
-        {
-            dots[slideNum - 1].style.backgroundColor = "#943f42";
-        }
-        else {
-            if (dots[i].hasAttribute("style"))
-            {
-                dots[i].removeAttribute("style");
-            }
-        }
-    }
-    /*
-    if (slideNum < 2) {
-        dots[slideNum - 2].removeAttribute("style");
-    }
-    else {
-        if (dots[dots.length - 1].hasAttribute("style")) {
-            dots[dots.length - 1].removeAttribute("style");
-        }
-    }*/
+    if (!slideNum) {
+        slideNum = totalSlides - 1;
+    } else slideNum--;
+    changeSlide();
 }
 
-/*
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-function currentSlide(n) {
-showSlides(slideIndex = n);
-}
-  
-function showSlides(n) {
-let i;
-let slides = document.getElementsByClassName("mySlides");
-let dots = document.getElementsByClassName("dot");
-if (n > slides.length) {slideIndex = 1}    
-if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-}
-for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-}
-slides[slideIndex-1].style.display = "block";  
-dots[slideIndex-1].className += " active";
+function resetTimer() {
+    // restart carousel-bar animation
+    bar.classList.remove("carousel-bar-fill");
+    bar.offsetWidth;   // this property recomputes the document's style, effectively serving as a means to restart the animation
+    bar.classList.add("carousel-bar-fill");
+
+    // reset timer
+    clearInterval(slideTimer);
+    slideTimer = setInterval(nextSlide, slideDuration);
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
-*/
+/////////////////////////////////////////////////
+// EVENT LISTENERS
+/////////////////////////////////////////////////
+
+document.querySelector(".carousel-button-prev").addEventListener("click", () => {
+    resetTimer();
+    previousSlide();
+});
+
+document.querySelector(".carousel-button-next").addEventListener("click", () => {
+    resetTimer();
+    nextSlide();
+});
+
+dots.forEach((dot, i) => dot.addEventListener("click", () => {
+    resetTimer();
+    slideNum = i;
+    changeSlide();
+}))
+
+/////////////////////////////////////////////////
+// ON LOAD
+/////////////////////////////////////////////////
+
+// set timer
+let slideTimer = setInterval(nextSlide, slideDuration);
+
+// set slide bar animation duration
+bar.style.animationDuration = (slideDuration / 1000) + "s";
+
+// activate carousel
+changeSlide();
